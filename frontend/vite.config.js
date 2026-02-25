77a6099bcd5637443aeb942994bcb62b527bee4d
Dragutu Maria <mariadragutu6@gmail.com>
2026-02-25 18:33:44 +0200

    adding blueprints

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,3 @@
-cat << 'EOF' > vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -9,4 +8,3 @@ export default defineConfig({
     tailwindcss(),
   ],
 })
-EOF
